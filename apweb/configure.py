0e1fe4651c3aa16a966255efa24856c9043cdf35
Adam Terrey <arterrey@gmail.com>
2019-08-16 15:50:21 +0700

    Add ++frontend++ static route

--- apweb/configure.py
+++ apweb/configure.py
@@ -2,4 +2,13 @@
 
 
 def includeme(config):
-    """Pyramid configuration hook for apweb package"""
+    """Pyramid configuration hook for apweb package
+
+    Configures the application in the following way:
+
+    - Adds a /++frontend++ route
+    """
+    settings = config.get_settings()
+
+    # Add static route
+    config.add_static_view('++frontend++', settings['frontend_static_location'])
--- apweb/configure_test.py
+++ apweb/configure_test.py
@@ -1,9 +1,19 @@
 # -*- coding:utf-8 -*-
 
+from . import configure
 from unittest import TestCase
 from unittest.mock import MagicMock
 
 
-class TestIncludeMe(TestCase):
+class TestIncludeme(TestCase):
     def setUp(self):
+        self.settings = {
+            'frontend_static_location': '/foo/bar'
+        }
         self.config = MagicMock()
+        self.config.get_settings.return_value = self.settings
+
+    def test_includeme(self):
+        configure.includeme(self.config)
+        c = self.config
+        c.add_static_view.assert_called_with('++frontend++', '/foo/bar')

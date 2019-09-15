81c715a465b456a10eb0314562753166983a5b1c
Adam Terrey <arterrey@gmail.com>
2019-09-15 18:48:32 +0700

    Add mailer and transaction manager to site

--- apweb/site/site.py
+++ apweb/site/site.py
@@ -5,3 +5,8 @@ import contextplus
 
 class Site(contextplus.Site):
     """A primitive site"""
+
+    def __init__(self, *args, mailer=None, transaction_manager=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mailer = mailer
+        self.transaction_manager = transaction_manager
--- apweb/site/site_test.py
+++ apweb/site/site_test.py
@@ -2,9 +2,13 @@
 
 from .site import Site
 from unittest import TestCase
+from unittest.mock import MagicMock
 
 
 class TestSite(TestCase):
     def test_init(self):
-        site = Site()
-        self.assertIsNotNone(site)
+        mailer = MagicMock()
+        tm = MagicMock()
+        site = Site(mailer=mailer, transaction_manager=tm)
+        self.assertEqual(site.mailer, mailer)
+        self.assertEqual(site.transaction_manager, tm)

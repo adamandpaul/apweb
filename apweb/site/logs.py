4dc077c560cfddd24119f22741655f2ce8f44608
Adam Terrey <arterrey@gmail.com>
2019-09-16 21:24:09 +1000

    Add logs functionality to site

---
+++ apweb/site/logs.py
@@ -0,0 +1,108 @@
+# -*- coding:utf-8 -*-
+
+from . import orm
+from contextplus import record_property
+from contextplus import SQLAlchemyCollection
+from contextplus import SQLAlchemyItem
+from datetime import datetime
+from datetime import timedelta
+from uuid import UUID
+from uuid import uuid4
+
+
+CRITICAL = 50
+ERROR = 40
+WARNING = 30
+INFO = 20
+DEBUG = 10
+
+LEVEL_NAMES = {
+    CRITICAL: "CRITICAL",
+    ERROR: "ERROR",
+    WARNING: "WARNING",
+    INFO: "INFO",
+    DEBUG: "DEBUG",
+}
+
+
+class LogEntry(SQLAlchemyItem):
+
+    record_type = orm.LogEntry
+    id_fields = ("log_entry_id",)
+    log_entry_id = record_property("log_entry_id")
+    timestamp = record_property("timestamp")
+    component = record_property("component")
+    level = record_property("level")
+    message = record_property("message")
+
+    @property
+    def readable_level(self):
+        return LEVEL_NAMES.get(self.level, str(self.level))
+
+    @property
+    def title(self):
+        return f"{self.timestamp}: {self.component}"
+
+    @property
+    def description(self):
+        return f"{self.readable_level}: {self.message}"
+
+
+class LogEntryCollection(SQLAlchemyCollection):
+    child_type = LogEntry
+    default_order_by_fields = ["timestamp desc"]
+
+    def name_from_child(self, child):
+        return str(child.log_entry_id)
+
+    def id_from_name(self, name):
+        try:
+            log_entry_id = UUID(name)
+        except ValueError as e:
+            raise TypeError("Name is not a valid uuid") from e
+        if name != str(log_entry_id):
+            raise TypeError("Incorrectly formatted uuid")
+        return {"log_entry_id": log_entry_id}
+
+    def iter_recent_logs(self):
+        """Return logs within the past day"""
+        before = datetime.utcnow() - timedelta(days=1)
+        q = self.query()
+        q = q.filter(orm.LogEntry.timestamp >= before)
+        q = q.order_by(orm.LogEntry.timestamp.desc())
+        for record in q:
+            yield self.child_from_record(record)
+
+
+class ComponentLogger(LogEntryCollection):
+    """A logger for a component"""
+
+    title = "Logger"
+
+    @property
+    def description(self):
+        return f'Component logger for component key "{self.component}"'
+
+    def __init__(self, parent=None, name: str = None, component: str = None):
+        """Create a component logger"""
+        super().__init__(parent=parent, name=name)
+        assert isinstance(component, str)
+        self.component = component
+
+    def query(self, *args, **kwargs):
+        return super().query(*args, **kwargs).filter_by(component=self.component)
+
+    def add(self, level, message):
+        """Create a log message"""
+        record = orm.LogEntry(
+            log_entry_id=uuid4(),
+            timestamp=datetime.utcnow(),
+            level=level,
+            component=self.component,
+            message=message,
+        )
+        self.acquire.db_session.add(record)
+
+    def info(self, message):
+        """Log an info level message to the log_entry table"""
+        self.add(INFO, message)
---
+++ apweb/site/logs_test.py
@@ -0,0 +1,77 @@
+# -*- coding:utf-8 -*-
+
+from . import logs
+from . import orm
+from datetime import datetime
+from unittest import TestCase
+from unittest.mock import MagicMock
+from unittest.mock import patch
+from uuid import UUID
+
+
+class TestLogEntry(TestCase):
+    def setUp(self):
+        self.record = orm.LogEntry(
+            log_entry_id=UUID("089d78b2-a55c-485e-9df6-a8ad5f16b991"),
+            timestamp=datetime(2000, 1, 2),
+            component="widget",
+            level=20,
+            message="Hi",
+        )
+        self.entry = logs.LogEntry(record=self.record)
+
+    def test_properties(self):
+        e = self.entry
+        self.assertEqual(e.log_entry_id, UUID("089d78b2-a55c-485e-9df6-a8ad5f16b991"))
+        self.assertEqual(e.readable_level, "INFO")
+        self.assertEqual(e.title, "2000-01-02 00:00:00: widget")
+        self.assertEqual(e.description, "INFO: Hi")
+
+
+class TestLogEntryCollection(TestCase):
+    def setUp(self):
+        self.collection = logs.LogEntryCollection()
+
+    def test_name_from_child(self):
+        child = MagicMock()
+        child.log_entry_id = UUID("089d78b2-a55c-485e-9df6-a8ad5f16b991")
+        result = self.collection.name_from_child(child)
+        self.assertEqual(result, "089d78b2-a55c-485e-9df6-a8ad5f16b991")
+
+    def test_id_from_name(self):
+        result = self.collection.id_from_name("089d78b2-a55c-485e-9df6-a8ad5f16b991")
+        self.assertEqual(
+            result, {"log_entry_id": UUID("089d78b2-a55c-485e-9df6-a8ad5f16b991")}
+        )
+
+    def test_iter_recent_logs(self):
+        self.collection.query = MagicMock()
+        self.collection.child_from_record = MagicMock()
+        qresults = ["foo", "bar"]
+        self.collection.query.return_value.filter.return_value.order_by.return_value = (
+            qresults
+        )
+        results = list(self.collection.iter_recent_logs())
+        self.collection.child_from_record.assert_any_call("foo")
+        self.collection.child_from_record.assert_any_call("bar")
+        self.assertEqual(len(results), 2)
+
+
+class TestComponentLogger(TestCase):
+    def setUp(self):
+        self.logger = logs.ComponentLogger(component="user")
+        self.logger.db_session = MagicMock()
+
+    @patch("apweb.site.logs.datetime")
+    @patch("apweb.site.logs.uuid4")
+    @patch("apweb.site.orm.LogEntry")
+    def test_add(self, LogEntry, uuid4, datetime):
+        self.logger.add(44, "Hello")
+        LogEntry.assert_called_with(
+            log_entry_id=uuid4.return_value,
+            timestamp=datetime.utcnow.return_value,
+            level=44,
+            component="user",
+            message="Hello",
+        )
+        self.logger.db_session.add.assert_called_with(LogEntry.return_value)

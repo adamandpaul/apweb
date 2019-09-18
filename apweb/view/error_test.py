e47d42cea4ffd10a2ac5919f9a27d86043759d4e
Adam Terrey <arterrey@gmail.com>
2019-09-18 15:25:42 +1000

    Fix code styles

--- apweb/view/error_test.py
+++ apweb/view/error_test.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 
-from . import error_production
 from . import error_develop
+from . import error_production
+from pyramid.httpexceptions import HTTPNotFound
 from unittest import TestCase
 from unittest.mock import MagicMock
-from pyramid.httpexceptions import HTTPNotFound
 
 
 class TestErrorView(object):
@@ -21,8 +21,10 @@ class TestErrorView(object):
         result = self.module.error(HTTPNotFound(), request)
         self.assertEqual(request.response.status_code, 404)
 
+
 class TestErrorProduction(TestErrorView, TestCase):
     module = error_production
+
 
 class TestErrorDevelop(TestErrorView, TestCase):
     module = error_develop

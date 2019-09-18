3252c5a62aea529c819fc49fd9b1a83a6f6a2dd3
Adam Terrey <arterrey@gmail.com>
2019-09-18 15:14:46 +1000

    Add view routes api, test and check

--- apweb/view/configure.py
+++ apweb/view/configure.py
@@ -3,6 +3,11 @@
 
 def includeme(config):
     """Configure view"""
-    config.add_route("api", "/api")
-    config.register_template_layer('apweb.view:templates')
-    config.scan()
+    config.add_route("api", "/api/*traverse")
+    config.add_route("check", "/_check/*traverse")
+    config.add_route("test", "/_test/*traverse")
+    config.register_template_layer("apweb.view:templates")
+    if config.registry["is_develop"]:
+        config.scan(ignore="apweb.view.error_production")
+    else:
+        config.scan(ignore="apweb.view.error_develop")
--- apweb/view/configure_test.py
+++ apweb/view/configure_test.py
@@ -8,7 +8,10 @@ from unittest.mock import MagicMock
 class TestConfigure(TestCase):
     def test_includeme(self):
         config = MagicMock()
+        config.registry = {"is_develop": False}
         configure.includeme(config)
-        config.register_template_layer.assert_called_with('apweb.view:templates')
-        config.add_route.assert_called_with("api", "/api")
-        config.scan.assert_called_with()
+        config.register_template_layer.assert_called_with("apweb.view:templates")
+        config.add_route.assert_any_call("api", "/api/*traverse")
+        config.add_route.assert_any_call("check", "/_check/*traverse")
+        config.add_route.assert_any_call("test", "/_test/*traverse")
+        config.scan.assert_called_with(ignore="apweb.view.error_develop")

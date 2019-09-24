aad311b9bca5a25852c43bfa7b981f41c02dedf6
Adam Terrey <arterrey@gmail.com>
2019-09-25 07:45:12 +1000

    Add test for request metods for site

--- apweb/site/configure.py
+++ apweb/site/configure.py
@@ -9,9 +9,23 @@ def site_factory(request):
     return site.Site.from_request(request)
 
 
+def get_user_for_unauthenticated_userid(request):
+    email = request.unauthenticated_userid
+    return request.site["users"].get_user_by_email(email)
+
+
+def get_roles(request):
+    user = request.user
+    if user:
+        return user.assigned_roles
+    return []
+
+
 def includeme(config):
     """A site configureation"""
     config.include("apweb")
     config.add_request_method(site_factory, "site", reify=True)
+    config.add_request_method(get_user_for_unauthenticated_userid, "user", reify=True)
+    config.add_request_method(get_roles, "roles", reify=True)
     config.register_login_provider(PasswordLoginProvider())
     config.commit()
--- apweb/site/configure_test.py
+++ apweb/site/configure_test.py
@@ -14,14 +14,35 @@ class TestSiteFactory(TestCase):
         Site.from_request.assert_called_with(request)
 
 
+class TestRequestMethods(TestCase):
+    def test_get_user_for_unauthenticated_userid(self):
+        request = MagicMock()
+        request.unauthenticated_userid = 'foo@blah.com'
+        result = configure.get_user_for_unauthenticated_userid(request)
+        self.assertEqual(result, request.site["users"].get_user_by_email.return_value)
+        request.site["users"].get_user_by_email.assert_called_with('foo@blah.com')
+
+    def test_get_roles(self):
+        request = MagicMock()
+        request.user.assigned_roles = ['one', 'two']
+        result = configure.get_roles(request)
+        self.assertEqual(result, ['one', 'two'])
+
+
 class TestIncludeme(TestCase):
     @patch("apweb.site.configure.PasswordLoginProvider")
     def test_includeme(self, PasswordLoginProvider):
         config = MagicMock()
         configure.includeme(config)
         config.include.assert_called_with("apweb")
-        config.add_request_method.assert_called_with(
+        config.add_request_method.assert_any_call(
             configure.site_factory, "site", reify=True
+        )
+        config.add_request_method.assert_any_call(
+            configure.get_user_for_unauthenticated_userid, "user", reify=True
+        )
+        config.add_request_method.assert_any_call(
+            configure.get_roles, "roles", reify=True
         )
         config.register_login_provider.assert_called_with(
             PasswordLoginProvider.return_value

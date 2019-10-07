bf1932bd9f50904bc885aba66fd4f16dd34600a7
Adam Terrey <arterrey@gmail.com>
2019-10-07 21:39:57 +1100

    Add breadcrumpbs to admin api view

--- apweb/site/view/resource.py
+++ apweb/site/view/resource.py
@@ -9,6 +9,42 @@ class ResourceView(object):
         self.context = context
         self.request = request
 
+    @view_config(
+        route_name="api", renderer="jsend", permission="view", request_method="GET"
+    )
+    def view(self):
+        return self.info
+
+    @view_config(
+        route_name="api",
+        name="view-manage",
+        renderer="jsend",
+        permission="manage",
+        request_method="GET",
+    )
+    def view_manage(self):
+        return {**self.view(), **self.info_manage}
+
+    @view_config(
+        route_name="api",
+        name="view-admin",
+        renderer="jsend",
+        permission="admin-access",
+        request_method="GET",
+    )
+    def view_admin(self):
+        return {**self.view_manage(), "admin": self.info_admin}
+
+    @view_config(
+        route_name="api",
+        name="view-debug",
+        renderer="jsend",
+        permission="debug",
+        request_method="GET",
+    )
+    def view_debug(self):
+        return {**self.view_admin(), "debug": self.info_debug}
+
     @reify
     def info(self):
         """Public Information"""
@@ -22,45 +58,31 @@ class ResourceView(object):
     @reify
     def info_admin(self):
         """Information for people who are responsible for the system"""
-        return {}
+        return {"breadcrumbs": self.breadcrumbs_admin}
 
     @reify
     def info_debug(self):
         """People who need to have internals for debugging"""
         return {}
 
-    @view_config(
-        route_name="api", renderer="jsend", permission="view", request_method="GET"
-    )
-    def view(self):
-        return self.info
+    @reify
+    def breadcrumbs_admin(self):
+        """Breadcrumbs which include named resources of each ancestor"""
+        breadcrumbs = []
+        resources = [self.context, *self.context.iter_ancestors()]
+        resources.reverse()
+        for resource in resources:
+            named_resources = []
+            for named_resource in resource.iter_named_resources():
+                named_resources.append(
+                    {"title": named_resource.title, "path": named_resource.path_names}
+                )
 
-    @view_config(
-        route_name="api",
-        name="view-manage",
-        renderer="jsend",
-        permission="manage",
-        request_method="get",
-    )
-    def view_manage(self):
-        return {**self.view(), **self.info_manage}
-
-    @view_config(
-        route_name="api",
-        name="view-admin",
-        renderer="jsend",
-        permission="admin-access",
-        request_method="get",
-    )
-    def view_admin(self):
-        return {**self.view_manage(), **self.info_admin}
-
-    @view_config(
-        route_name="api",
-        name="view-debug",
-        renderer="jsend",
-        permission="debug",
-        request_method="get",
-    )
-    def view_debug(self):
-        return {**self.view_admin(), **self.info_debug}
+            breadcrumbs.append(
+                {
+                    "title": resource.title,
+                    "path": resource.path_names,
+                    "named_resources": named_resources,
+                }
+            )
+        return breadcrumbs
--- apweb/site/view/resource_test.py
+++ apweb/site/view/resource_test.py
@@ -12,9 +12,10 @@ class TestResourceView(TestCase):
         self.view = resource.ResourceView(self.context, self.request)
 
     def test_infos(self):
+        self.view.__dict__["breadcrumbs_admin"] = ["app", "foo"]
         self.assertEqual(self.view.info, {})
         self.assertEqual(self.view.info_manage, {})
-        self.assertEqual(self.view.info_admin, {})
+        self.assertEqual(self.view.info_admin, {"breadcrumbs": ["app", "foo"]})
         self.assertEqual(self.view.info_debug, {})
 
     def test_gets(self):
@@ -29,9 +30,52 @@ class TestResourceView(TestCase):
         self.assertEqual(self.view.view(), {"a": "Info"})
         self.assertEqual(self.view.view_manage(), {"a": "Info", "b": "Info Manage"})
         self.assertEqual(
-            self.view.view_admin(), {"a": "Info", "b": "Info Manage", "c": "Info Admin"}
+            self.view.view_admin(),
+            {"a": "Info", "b": "Info Manage", "admin": {"c": "Info Admin"}},
         )
         self.assertEqual(
             self.view.view_debug(),
-            {"a": "Info", "b": "Info Manage", "c": "Info Admin", "d": "Info Debug"},
+            {
+                "a": "Info",
+                "b": "Info Manage",
+                "admin": {"c": "Info Admin"},
+                "debug": {"d": "Info Debug"},
+            },
         )
+
+
+class TestResourceBreadcrumbsView(TestCase):
+    def setUp(self):
+
+        root_named_resource = MagicMock()
+        root_named_resource.title = "N"
+        root_named_resource.path_names = ["", "n"]
+
+        root = MagicMock()
+        root.title = "A"
+        root.path_names = [""]
+        root.iter_named_resources.return_value = [root_named_resource]
+        parent = MagicMock()
+        parent.title = "P"
+        parent.path_names = ["", "p"]
+        context = MagicMock()
+        context.title = "C"
+        context.path_names = ["", "p", "c"]
+        context.iter_ancestors.return_value = [parent, root]
+
+        self.request = MagicMock()
+        self.context = context
+        self.view = resource.ResourceView(self.context, self.request)
+
+    def test_breadcrumbs_admin(self):
+        result = self.view.breadcrumbs_admin
+        expected = [
+            {
+                "title": "A",
+                "path": [""],
+                "named_resources": [{"title": "N", "path": ["", "n"]}],
+            },
+            {"title": "P", "path": ["", "p"], "named_resources": []},
+            {"title": "C", "path": ["", "p", "c"], "named_resources": []},
+        ]
+        self.assertEqual(result, expected)

61564b4ad67632630882ca3d35791b6f567d8e8a
Adam Terrey <arterrey@gmail.com>
2020-01-06 15:23:29 +0700

    Add resource tile view

---
+++ vuejs/admin/ResourceTile.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <component :is="ui" :tile="tile" /> 
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        tile: {},
+    },
+    computed: {
+        ui() {
+            return this.tile.ui || "resource-tile-default"
+        },
+    },
+}
+</script>

ece0d1b6c976775ada0071648b6dc68cac10a07b
Adam Terrey <arterrey@gmail.com>
2020-02-12 16:12:17 +0700

    Add summary values to site object

--- apweb/site/view/site.py
+++ apweb/site/view/site.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from .resource import ResourceView
+from apweb.utils import map_context_reify
 from pyramid.decorator import reify
 from pyramid.view import view_defaults
 from venusian import lift
@@ -8,5 +9,16 @@ from venusian import lift
 
 @lift()
 @view_defaults(context="apweb.site.resource.Site")
+@map_context_reify(
+    "application_url",
+    "application_deployment",
+)
 class SiteView(ResourceView):
-    pass
+
+    @reify
+    def admin_summary(self):
+        return [
+            *super().admin_summary,
+            {"title": "Application URL", "value": self.application_url},
+            {"title": "Deployment", "value": self.application_deployment},
+        ]

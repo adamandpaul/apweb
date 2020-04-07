41112f8d3dea3acfd31bf76fd176155f8b12acb1
Jon Walsh <jon.walsh@adamandpaul.biz>
2020-04-07 07:06:05 +0000

    Fix for enum properties with no default value in JSON schema

--- apweb/site/view/utils.py
+++ apweb/site/view/utils.py
@@ -17,7 +17,8 @@ def serve_schema(schema, required=True):
     # JSON schema defaults to not accepting null values, and UI can't distinguish them from false
     # So map None -> False/'', otherwise trying to validate input later will raise an error
     for prop in schema["properties"].values():
-        if prop.get("default") is None:
+        # WARN Don't apply for enums as an empty string is sure to not be a valid option
+        if prop.get("default") is None and "enum" not in prop:
             prop["default"] = False if prop["type"] == "boolean" else ""
 
     return schema

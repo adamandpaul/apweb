cfaa98aa41973731bd3b120c9194c85968cdf1a6
andrew-houghton <houghtonandrew0@gmail.com>
2020-08-19 11:11:08 +1000

    Allow PATCH (#5)

--- apweb/view/api_cors.py
+++ apweb/view/api_cors.py
@@ -6,7 +6,7 @@ from pyramid.view import view_config
 
 DEFAULT_CORS_HEADERS = {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "POST,GET,DELETE,PUT,OPTIONS",
+    "Access-Control-Allow-Methods": "POST,PATCH,GET,DELETE,PUT,OPTIONS",
     "Access-Control-Allow-Headers": "Origin, Content-Type, Accept, Authorization",
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Max-Age": "3600",
